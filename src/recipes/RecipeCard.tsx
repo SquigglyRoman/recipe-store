dc162cb2f4359a6348b1422cb5019dd596212c1f
SquigglyRoman <rohen04@gmail.com>
2024-06-17 18:58:36 +0200

    Add show recipe button

--- src/recipes/RecipeCard.tsx
+++ src/recipes/RecipeCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { Recipe } from './models';
-import { Badge } from 'react-bootstrap';
+import { Badge, Button } from 'react-bootstrap';
 
 interface RecipeCardProps {
     recipe: Recipe;
@@ -13,11 +13,16 @@ const RecipeCard: React.FC<RecipeCardProps> = ({ recipe }) => {
             <Card.Img variant="top" src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
             <Card.Body>
                 <Card.Title>{recipe.metadata.name}</Card.Title>
-                <div style={{display: 'flex', gap: '5px'}}>
-                {recipe.metadata.tags.map(tag => (
-                    <Badge key={tag}>{tag}</Badge>
-                ))}
-                </div>
+                <Card.Text>
+                    <div style={{ display: 'flex', gap: '5px' }}>
+                        {recipe.metadata.tags.map(tag => (
+                            <Badge key={tag} bg="secondary">{tag}</Badge>
+                        ))}
+                    </div>
+                </Card.Text>
+                <a href={recipe.fileUrl}>
+                    <Button>Show recipe</Button>
+                </a>
             </Card.Body>
         </Card>
     );
