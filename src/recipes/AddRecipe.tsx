f4662e2883a2e3d6e8aee3bd6c8675fbde44f4cf
SquigglyRoman <rohen04@gmail.com>
2024-07-13 15:50:02 +0200

    Add little icon

--- src/recipes/AddRecipe.tsx
+++ src/recipes/AddRecipe.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import { BsPlus } from 'react-icons/bs';
 import AddRecipeDialogue from './AddRecipeDialogue';
 
 const AddRecipe: React.FC = () => {
@@ -7,8 +8,9 @@ const AddRecipe: React.FC = () => {
 
     return (
         <div>
-            <Button variant="outline-primary" onClick={() => setShowDialogue(true)}>
+            <Button className="d-flex flex-row align-items-center gap-1" variant="outline-primary" onClick={() => setShowDialogue(true)}>
                 Add Recipe
+                <BsPlus />
             </Button>
             <AddRecipeDialogue show={showDialogue} onHide={() => setShowDialogue(false)} />
         </div>
